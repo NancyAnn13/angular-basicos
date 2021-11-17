63ce78fed2338b8f2c12380bcc67310e58e36fa9
NancyAnn13 <nancy.solis@sunustech.com>
2021-11-17 11:37:26 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { contadormodule } from './contador/contador.module';
+import { heroesmodule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    heroesmodule,
+    contadormodule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+
+
+})
+
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman','Iromnan','Hulk','Thor','Capitan Ameria'];
+  heroeBorrado: string='';
+
+  BorrarHeroe()
+  {
+    this.heroeBorrado = this.heroes.shift() || '';
+    
+    //console.log(HeroeBorrado);
+
+  } 
+}
